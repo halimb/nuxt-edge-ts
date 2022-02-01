6a1afc59312c72ea373a88309160265ee00f8f21
halimb <halim@evercam.io>
2022-02-01 18:13:01 +0000

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,6 @@
+import { defineNuxtConfig } from '@nuxt/bridge'
+
+export default defineNuxtConfig({
+  ssr: false,
+  target: 'static',
+})
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>{{helloWorld}}</div>
+</template>
+
+<script lang="ts">
+export default {
+  computed: {
+    // Should cause a typescript error
+    helloWorld(): number {
+      return "Hello world"
+    }
+  }
+}
+</script>
